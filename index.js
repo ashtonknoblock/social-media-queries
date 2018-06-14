5a8f0a8e1554252bed70fcfd99b4113b8bb7d296
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-06-14 14:33:13 -0400

    first commit, completed

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Client } = require('pg');
+const express = require('express');
+var bodyParser = require('body-parser');
+
+// create an express application
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// create a postgresql client
+const client = new Client({
+    database: 'social-media'
+});
+
+// route handlers go here
+app.get('/users', (req, res) => {
+    client.query('SELECT * FROM users', (err, result) => {
+        res.send(result.rows);
+    });
+});
+
+app.post('/users', (req, res) => {
+const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
+const values = [req.body.username, req.body.bio];
+client.query(text, values, (err, result) => {
+    res.send(result.rows[0]);
+});
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = 'SELECT * FROM users WHERE id = ($1)' 
+    const UserID = [req.params.id];
+    client.query(id, UserID, (err,result) => {
+        console.log(result)
+        res.send(result.rows[0]);
+    })
+});
+
+// start a server that listens on port 3000 and connects the sql client on success
+app.listen(3000, () => {
+    client.connect();
+});
---
+++ queries/04_insert_posts.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts (title, body )
+ VALUES 
+ ('admin announcement', 'This API is awesome!'),
+ ('just a test', 'Just a test. I repeat, just a test!')
+ ;
